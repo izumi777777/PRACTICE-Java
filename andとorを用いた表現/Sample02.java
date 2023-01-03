2816524a11e3e9840ccedd5a061462aaae5107e3
kikumura01 <kikumura804@gmail.com>
2023-01-03 16:53:09 +0900

    test

---
+++ andとorを用いた表現/Sample02.java
@@ -0,0 +1,10 @@
+package andとorを用いた表現;
+
+public class Sample02 {
+    public static void main(String[] args) {
+        int n = 8;
+        if(n > 0 && n%2 == 0) 
+        System.out.println("nは整数の偶数です");
+    }
+    
+}

12fd037268b8cf8081e37ff1559fa9972d177e58
kikumura01 <kikumura804@gmail.com>
2023-01-03 20:16:31 +0900

    test

--- practice/random/RandomNumber.java
+++ practice/random/RandomNumber.java
@@ -6,7 +6,7 @@ public class RandomNumber {
     public static void main(String[] args) {
         // 変数の宣言
         double r = Math.random();
-        int i = (int)(r*100);
+        int i = (int)(r*10);
         
         // 変数rに格納されたランダムな整数を表示する
         System.out.println(i);

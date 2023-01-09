279b173f6266fcf3cd4184ff550fbe2c26be09f7
kikumura01 <kikumura804@gmail.com>
2023-01-09 22:32:01 +0900

    20230109

---
+++ practice/復習/SamppeIf.java
@@ -0,0 +1,19 @@
+package practice.復習;
+
+public class SamppeIf {
+    public static void main(String[] args) {
+        int n = 0;
+        if(n==0)System.out.println("nはゼロです");
+    }
+    
+}
+
+/*式の左辺と右辺が等しいか判定するには「==」と、イコールを２つ記述します。※イコール１つ（=）では変数に値を代入する処理になる。
+ * このプログラムでは５行目で変数nの初期値に0が入り、6行目の「n == 0」の判定が成り立つ（true）ので、
+ * System.out.printlnの処理が実行される。
+ * 
+ * 5行目のnの初期値を0以外にすると、「nはゼロです」と出力さなくなることを確認しましょう。
+ * 
+ * 
+ * 
+ */

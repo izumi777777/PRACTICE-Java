7c55f1cc9f9d13b6db0ed435880db496ed0fd12b
kikumura01 <kikumura804@gmail.com>
2023-09-20 00:34:00 +0900

    First Commit

---
+++ 四則演算/java1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class java1 {
+    public static void main(String[] args) {
+        // 足し算
+        int a = 100;
+        int b = 200;
+        System.out.println(a+b);
+
+        // 引き算
+        System.out.println(a-b);
+
+        // 掛け算を行う
+        System.out.println(a*b);
+
+        // 割り算を行う
+        double x = 100;
+        double y = 200;
+        System.out.println(x/y);
+    } 
+}

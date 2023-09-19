e2d23fc00fff84bb85949e794ff2f1edb831e547
kikumura01 <kikumura804@gmail.com>
2023-09-20 00:43:33 +0900

    first commit

---
+++ 四則演算/java2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class java2 {
+    public static void main(String[] args) {
+        // aの初期値を書き換えて、その値が出力されることを確認しましょう。
+        int a = 100;
+        System.out.println(a);
+    }
+}
